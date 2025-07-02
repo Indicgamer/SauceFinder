538bb95c6a16bbecaee938cfabe6e96f4f8fc767
Indicgamer <channayyagarampalli@gmail.com>
2025-07-02 23:14:35 +0530

    view engine update

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileupload());
